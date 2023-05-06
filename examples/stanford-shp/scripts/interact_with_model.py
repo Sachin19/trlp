d322124bb0bdac511ad405536dd12e4f9557d7df
Sachin Kumar <kumar.sachin52@gmail.com>
2023-05-06 02:28:13 -0400

    interact with model, starter for evaluations script

---
+++ examples/stanford-shp/scripts/interact_with_model.py
@@ -0,0 +1,62 @@
+# coding=utf-8
+# Copyright 2022 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass, field
+from typing import Optional
+
+import torch
+from datasets import load_dataset
+
+import transformers
+from transformers import AutoTokenizer, HfArgumentParser, AutoModelForSeq2SeqLM
+
+
+@dataclass
+class ScriptArguments:
+    """
+    The name of the Casual LM model we wish to fine with PPO
+    """
+
+    # NOTE: gpt2 models use Conv1D instead of Linear layers which are not yet supported in 8 bit mode
+    # models like gpt-neo* models are more suitable.
+    model_name: Optional[str] = field(default="/projects/tir6/general/sachink/personalized-LM/2023/models/flan-t5-large-rerun-legaladvice_rl_step_25", metadata={"help": "the reward model name"})
+
+
+parser = HfArgumentParser(ScriptArguments)
+script_args = parser.parse_args_into_dataclasses()[0]
+
+# Now let's build the model, the reference model, and the tokenizer.
+model = AutoModelForSeq2SeqLM.from_pretrained(script_args.model_name)
+tokenizer = AutoTokenizer.from_pretrained(script_args.model_name)
+
+generation_kwargs = {"top_k": 0.0, "top_p": 1.0, "do_sample": True, "eos_token_id": -1}
+
+
+device=0
+if not torch.cuda.is_available():
+    device="cpu"  
+
+while True:
+    x = input("Enter the prompt: ")
+    n = int(input("Output length: " ))
+
+    # input(batch)
+    query_tensors = tokenizer.encode(x, return_tensors="pt")
+
+    # Get response from t5
+    response_tensor = model.generate(query_tensors, max_new_tokens=n, **generation_kwargs)
+    response = tokenizer.decode(response_tensor[0])
+
+    print(response)
